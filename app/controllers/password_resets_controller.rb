fe062a70ad610d5710bfdca713aa00e039b89bf6
aakashnand <aakashnand@gmail.com>
2017-04-25 04:25:39 +0000

    Done till Destroy Micropost

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,9 +1,7 @@
 class PasswordResetsController < ApplicationController
-  
-  before_action :get_user,   only: [:edit, :update]
-  before_action :valid_user, only: [:edit, :update]
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
   before_action :check_expiration, only: [:edit, :update]    # Case (1)
-
 
   def new
   end
@@ -23,7 +21,7 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
-  
+
   def update
     if params[:user][:password].empty?                  # Case (3)
       @user.errors.add(:password, "can't be empty")
@@ -37,12 +35,14 @@ class PasswordResetsController < ApplicationController
       render 'edit'                                     # Case (2)
     end
   end
-  
+
   private
-   
+
     def user_params
       params.require(:user).permit(:password, :password_confirmation)
     end
+
+    # Before filters
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -55,11 +55,12 @@ class PasswordResetsController < ApplicationController
         redirect_to root_url
       end
     end
-    
-  def check_expiration
-    if @user.password_reset_expired?
-      flash[:danger] = "Password reset has expired."
-      redirect_to new_password_reset_url
+
+    # Checks expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
     end
-  end    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  
+ has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
+  
   
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
@@ -55,7 +58,7 @@ class User < ApplicationRecord
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-   update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
+    update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   # Sends password reset email.
@@ -65,6 +68,12 @@ class User < ApplicationRecord
   
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   
